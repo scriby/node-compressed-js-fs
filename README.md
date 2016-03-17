4c698c80bf1796fd544e0cc09663ff21072ef517
Chris Scribner <scriby@gmail.com>
2016-03-17 11:47:43 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-compressed-js-fs
 Use compressed JS on disk (because size matters sometimes)
+
+# Usage
+
+Prepare code by calling compressFile(filename, true, callback), which will replace .js files with a deflated version.
+Note that because this will replace the input files, you should make a copy of your source before running it.
+
+When running the code, call compressedJsFs.installHook() before requiring any other modules.
---
+++ index.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var zlib = require('zlib');
+var zlibjs = require('zlibjs'); //Using zlibjs for inflateSync on node v0.10.x
+var mod = require('module').prototype;
+var originalCompile = mod._compile;
+
+var hookInstalled = false;
+exports.installHook = function() {
+  if (!hookInstalled) {
+    mod._compile = function(content, filename) {
+      arguments[0] = exports.decompressFile(content, filename);
+      //console.log(arguments[0])
+      return originalCompile.apply(this, arguments);
+    };
+
+    hookInstalled = true;
+  }
+};
+
+exports.decompressFile = function(content, filename) {
+  if (content.charCodeAt(0) === 120 && content.charCodeAt(1) === 65533) {
+    var buffer = new Buffer(fs.readFileSync(filename));
+
+    return zlibjs.inflateSync(buffer).toString();
+  }
+
+  return content;
+};
+
+exports.compressFile = function(filename, replace, callback) {
+  if (typeof replace === 'function') {
+    callback = replace;
+    replace = false;
+  }
+
+  var deflate = zlib.createDeflate({
+    level: 7
+  });
+
+  var writeStream = fs.createWriteStream(filename + '.gz');
+  fs.createReadStream(filename).pipe(deflate).pipe(writeStream);
+
+  writeStream.on('close', function() {
+    writeStream.removeAllListeners();
+
+    if (replace) {
+      fs.rename(filename + '.gz', filename, callback);
+    } else {
+      callback();
+    }
+  });
+
+  writeStream.on('error', function(e) {
+    writeStream.removeAllListeners();
+    callback(e);
+  });
+};
+
+//exports.compressFile(__dirname + '/test.js', false, function(err) {
+//  if (err) {
+//    throw err;
+//  }
+//
+//  exports.installHook();
+//
+//  require('./test.js.gz');
+//});
